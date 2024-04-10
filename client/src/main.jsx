fa822990d8c04c678c700c5ba15fac81980d6065
Abdullah Al Noman <abdullahalnomancse@gmail.com>
2024-04-10 13:19:07 +0600

    added home and about apge

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route, Link, NavLink } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
 import "./style.css";
+import Home from "./pages/Home.jsx";
+import About from "./pages/About.jsx";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <NavLink to="/">#CARLIFE</NavLink>
+      <nav>
+        <NavLink to="/about">About</NavLink>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
